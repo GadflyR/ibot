045e92fca6cf89a3bb8d8174a1e25df112df4b90
GadflyR <ottomanhabsburg@gmail.com>
2024-12-28 11:25:00 -0500

    2

--- index.js
+++ index.js
@@ -2,45 +2,49 @@ function showabout(){
     $("#about_container").css("display","inherit");
     $("#about_container").addClass("animated slideInLeft");
     setTimeout(function(){
-        $("#about_container").removeClass("animated slideInLeft");
+      $("#about_container").removeClass("animated slideInLeft");
     },800);
-}
-function closeabout(){
+  }
+  function closeabout(){
     $("#about_container").addClass("animated slideOutLeft");
     setTimeout(function(){
-        $("#about_container").removeClass("animated slideOutLeft");
-        $("#about_container").css("display","none");
+      $("#about_container").removeClass("animated slideOutLeft");
+      $("#about_container").css("display","none");
     },800);
-}
-function showwork(){
+  }
+  
+  function showproduct(){
+    $("#product_container").css("display","inherit");
+    $("#product_container").addClass("animated slideInLeft");
+    setTimeout(function(){
+      $("#product_container").removeClass("animated slideInLeft");
+    },800);
+  }
+  function closeproduct(){
+    $("#product_container").addClass("animated slideOutLeft");
+    setTimeout(function(){
+      $("#product_container").removeClass("animated slideOutLeft");
+      $("#product_container").css("display","none");
+    },800);
+  }
+  
+  function showwork(){
     $("#work_container").css("display","inherit");
     $("#work_container").addClass("animated slideInRight");
     setTimeout(function(){
-        $("#work_container").removeClass("animated slideInRight");
+      $("#work_container").removeClass("animated slideInRight");
     },800);
-}
-function closework(){
+  }
+  function closework(){
     $("#work_container").addClass("animated slideOutRight");
     setTimeout(function(){
-        $("#work_container").removeClass("animated slideOutRight");
-        $("#work_container").css("display","none");
+      $("#work_container").removeClass("animated slideOutRight");
+      $("#work_container").css("display","none");
     },800);
-}
-function showcontact(){
-    $("#contact_container").css("display","inherit");
-    $("#contact_container").addClass("animated slideInUp");
-    setTimeout(function(){
-        $("#contact_container").removeClass("animated slideInUp");
-    },800);
-}
-function closecontact(){
-    $("#contact_container").addClass("animated slideOutDown");
-    setTimeout(function(){
-        $("#contact_container").removeClass("animated slideOutDown");
-        $("#contact_container").css("display","none");
-    },800);
-}
-setTimeout(function(){
+  }
+  
+  /* Loading fade-out */
+  setTimeout(function(){
     $("#loading").addClass("animated fadeOut");
     setTimeout(function(){
       $("#loading").removeClass("animated fadeOut");
@@ -49,5 +53,7 @@ setTimeout(function(){
       $("#about").removeClass("animated fadeIn");
       $("#contact").removeClass("animated fadeIn");
       $("#work").removeClass("animated fadeIn");
+      $("#product").removeClass("animated fadeIn");
     },1000);
-},1500);
+  },1500);
+  
